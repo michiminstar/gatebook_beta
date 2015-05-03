266c90ef4545057603a7f77be704e76276ddad4a
Michi Kanda <michi.kanda.0513@gmail.com>
2015-05-03 11:40:16 +0900

    Delete status posting function temporarily

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503023611) do
-
-  create_table "statuses", force: :cascade do |t|
-    t.text     "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
