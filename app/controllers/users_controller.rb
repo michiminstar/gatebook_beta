a1dea49692c650aa763c9dcb613e34b887e35ffc
Michi Kanda <michi.kanda.0513@gmail.com>
2015-05-03 23:21:22 +0900

    Add the User Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ようこそ、Gatebookへ！"
       log_in @user
       redirect_to @user
     else
@@ -17,9 +25,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールを更新しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインして下さい。"
+        redirect_to login_url
+      end
+    end
+
+     def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
